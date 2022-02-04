a7b131c7b5f56b917ad69b77764ec8b2d38bc182
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-02-04 01:50:14 -0300

    Validacao

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,14 @@
+import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsBoolean()
   admin: boolean;
 }
