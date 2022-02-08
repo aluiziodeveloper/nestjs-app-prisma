5544be09c78a303daa4f6123ac4be00d5019e23a
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-02-08 00:07:09 -0300

    UsersRepository

---
+++ src/users/repositories/users.repository.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UpdateUserDto } from '../dto/update-user.dto';
+import { UserEntity } from '../entities/user.entity';
+
+@Injectable()
+export class UsersRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto): Promise<UserEntity> {
+    return this.prisma.user.create({
+      data: createUserDto,
+    });
+  }
+
+  async findAll(): Promise<UserEntity[]> {
+    return await this.prisma.user.findMany();
+  }
+
+  async findOne(id: number): Promise<UserEntity> {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto): Promise<UserEntity> {
+    return this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: updateUserDto,
+    });
+  }
+
+  async remove(id: number): Promise<UserEntity> {
+    return this.prisma.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
