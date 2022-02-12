968e99e11104c3ee46421b93126ae70c5a769bc1
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-02-12 01:52:29 -0300

    database.interceptor - parte 2

--- src/common/errors/interceptors/database.interceptor.ts
+++ src/common/errors/interceptors/database.interceptor.ts
@@ -5,12 +5,15 @@ import {
   CallHandler,
 } from '@nestjs/common';
 import { catchError, Observable } from 'rxjs';
+import { isPrismaError } from '../utils/is-prisma-error.util';
 
 @Injectable()
 export class DatabaseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       catchError(error => {
+        if (isPrismaError(error)) {
+        }
         throw error;
       }),
     );
---
+++ src/common/errors/utils/is-prisma-error.util.ts
@@ -0,0 +1,9 @@
+import { PrismaClientError } from '../types/PrismaClientError';
+
+export const isPrismaError = (e: PrismaClientError) => {
+  return (
+    typeof e.code === 'string' &&
+    typeof e.clientVersion === 'string' &&
+    (typeof e.meta === 'undefined' || typeof e.meta === 'object')
+  );
+};
