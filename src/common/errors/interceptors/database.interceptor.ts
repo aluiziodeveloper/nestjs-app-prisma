8d2518efa3f60ac49d1989f0ed0f1b38c0fb0d3e
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-02-12 02:36:00 -0300

    database.interceptor - parte 5

--- src/common/errors/interceptors/database.interceptor.ts
+++ src/common/errors/interceptors/database.interceptor.ts
@@ -3,8 +3,11 @@ import {
   NestInterceptor,
   ExecutionContext,
   CallHandler,
+  BadRequestException,
 } from '@nestjs/common';
 import { catchError, Observable } from 'rxjs';
+import { DatabaseError } from '../types/DatabaseError';
+import { handleDatabaseErrors } from '../utils/handle-database-errors.util';
 import { isPrismaError } from '../utils/is-prisma-error.util';
 
 @Injectable()
@@ -13,8 +16,13 @@ export class DatabaseInterceptor implements NestInterceptor {
     return next.handle().pipe(
       catchError(error => {
         if (isPrismaError(error)) {
+          error = handleDatabaseErrors(error);
         }
-        throw error;
+        if (error instanceof DatabaseError) {
+          throw new BadRequestException(error.message);
+        } else {
+          throw error;
+        }
       }),
     );
   }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConflictInterceptor } from './common/errors/interceptors/conflict.interceptor';
+import { DatabaseInterceptor } from './common/errors/interceptors/database.interceptor';
 import { NotFoundInterceptor } from './common/errors/interceptors/notfound.interceptor';
 import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
 
@@ -14,6 +16,8 @@ async function bootstrap() {
     }),
   );
   //app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ConflictInterceptor());
+  app.useGlobalInterceptors(new DatabaseInterceptor());
   app.useGlobalInterceptors(new UnauthorizedInterceptor());
   app.useGlobalInterceptors(new NotFoundInterceptor());
   await app.listen(process.env.PORT || 3000);
