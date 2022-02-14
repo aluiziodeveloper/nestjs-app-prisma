15c35a2896626603b63fac02fe7344605878a8e2
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-02-14 17:31:40 -0300

    Posts Repository

---
+++ src/posts/repositories/posts.repository.ts
@@ -0,0 +1,43 @@
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreatePostDto } from '../dto/create-post.dto';
+import { UpdatePostDto } from '../dto/update-post.dto';
+import { PostEntity } from '../entities/post.entity';
+
+export class PostsRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createPostDto: CreatePostDto): Promise<PostEntity> {
+    return this.prisma.post.create({
+      data: createPostDto,
+    });
+  }
+
+  async findAll(): Promise<PostEntity[]> {
+    return await this.prisma.post.findMany();
+  }
+
+  async findOne(id: number): Promise<PostEntity> {
+    return this.prisma.post.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: number, updatePostDto: UpdatePostDto): Promise<PostEntity> {
+    return this.prisma.post.update({
+      where: {
+        id,
+      },
+      data: updatePostDto,
+    });
+  }
+
+  async remove(id: number): Promise<PostEntity> {
+    return this.prisma.post.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
