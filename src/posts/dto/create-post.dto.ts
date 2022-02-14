5d3e4b50dace69770e6aac8d4035e2b30be427a2
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-02-14 17:19:14 -0300

    Validacao - Post

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,5 +1,14 @@
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
 export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsOptional()
   content?: string;
+
+  @IsEmail()
   authorEmail: string;
 }
